745aa31d1c8e3efe4fb6fc6956f93740b401c378
Tatiana Scott <hello@tatianascott.com>
2022-01-16 12:09:39 -0500

    fix background

--- src/App.js
+++ src/App.js
@@ -18,16 +18,19 @@ function App() {
     audio.play()
   };
   return (
-    <div className="App">
-      <div className="card">
-        <img src={nanasphoto} alt='nana'/>
-        <h1 className="title">Nana Says</h1>
-        <h2 className="sub-title"> The HBIC and Deaconess</h2>
-        <p className="description">Well hello, what the John Brown is wrong with you? You must be dizzy if you never heard about the Magic 8 Ball? God is my Magic 8 Ball, and all. Alright now, go ahead and close your eyes, ask me whatever is on your mind and press the damn button.</p>
-        <p><button onClick={handleClick}>Ask Nana!*</button></p>
-        <h5 className="disclaimer">*DISCLAIMER: By participating in the 'Ask Nana' experience you are opting to subject yourself to blunt and unfiltered honesty. All responses expressed on this website are soley those of Nana and she does not care about your feelings. Ask at your own risk.</h5>
-      </div>
+
+      <div className="App">
+        <div className="card">
+          <img src={nanasphoto} alt='nana'/>
+          <h1 className="title">Nana Says</h1>
+          <h2 className="sub-title"> The HBIC and Deaconess</h2>
+          <p className="description">Well hello, what the John Brown is wrong with you? You must be dizzy if you never heard about the Magic 8 Ball? God is my Magic 8 Ball, and all. Alright now, go ahead and close your eyes, ask me whatever is on your mind and press the damn button.</p>
+          <p><button onClick={handleClick}>Ask Nana!*</button></p>
+          <h5 className="disclaimer">*DISCLAIMER: By participating in the 'Ask Nana' experience you are opting to subject yourself to blunt and unfiltered honesty. All responses expressed on this website are soley those of Nana and she does not care about your feelings. Ask at your own risk.</h5>
+        </div>
+
     </div>
+
   );
 }
 
