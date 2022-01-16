4c0fa285bd4643f1cba8bee2ec5149812b8d2358
Tatiana Scott <hello@tatianascott.com>
2022-01-15 22:16:55 -0500

    first commit

---
+++ src/voiceCollection.js
@@ -0,0 +1,10 @@
+const responses = {
+  1: "https://nanasays.s3.amazonaws.com/BeCarefulOutThereOk.m4a",
+  2: "https://nanasays.s3.amazonaws.com/CallMeBackLater.m4a",
+  3: "https://nanasays.s3.amazonaws.com/CongratulationsStinkWeed.m4a",
+  4: "https://nanasays.s3.amazonaws.com/HaveABlessedYear.m4a",
+  5: "https://nanasays.s3.amazonaws.com/HelloMyDarlingBaby.m4a",
+  6: "https://nanasays.s3.amazonaws.com/WishYouLotsOfLuckAndLove.m4a"
+};
+
+export default responses;
